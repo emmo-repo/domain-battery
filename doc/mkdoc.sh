1d960d616cdacf155f312c1d59cdf4a6b1f761be
Casper Welzel Andersen <casper.w.andersen@sintef.no>
2022-11-25 15:11:23 +0100

    Setup documentation build and CI/CD workflows

--- doc/mkdoc.sh
+++ doc/mkdoc.sh
@@ -13,7 +13,8 @@ mkdir -p ${tmpdir}/figs
 cp -u ${rootdir}/doc/img/bigmap.png ${tmpdir}/figs/.
 
 ontograph -m ${rootdir}/battery.ttl ${tmpdir}/battery-structure.png
-ontoconvert -si ${rootdir}/battery.ttl ${tmpdir}/battery-inferred.ttl
+ontoconvert -si -- ${rootdir}/battery.ttl ${tmpdir}/battery-inferred.ttl
+ontoconvert -si -- ${rootdir}/batteryquantities.ttl ${tmpdir}/batteryquantities-inferred.ttl
 
 ontodoc --template=battery.md --format=html ${tmpdir}/battery-inferred.ttl \
         ${tmpdir}/battery.html
