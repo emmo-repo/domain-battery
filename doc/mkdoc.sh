c18fee4570f8b04826762b78fd81f258fe223124
francescalb <francesca.l.bleken@sintef.no>
2023-06-20 08:11:41 +0200

    Removed Figuretext for empty Figure and commented usage of ingerred version in mkdocs

--- doc/mkdoc.sh
+++ doc/mkdoc.sh
@@ -13,6 +13,8 @@ mkdir -p ${tmpdir}/figs
 cp -u ${rootdir}/doc/img/bigmap.png ${tmpdir}/figs/.
 
 ontograph -m ${rootdir}/battery.ttl ${tmpdir}/battery-structure.png
+#ontoconvert -si -- ${rootdir}/battery.ttl ${tmpdir}/battery-inferred.ttl
+#ontoconvert -si -- ${rootdir}/batteryquantities.ttl ${tmpdir}/batteryquantities-inferred.ttl
 ontoconvert -s -- ${rootdir}/battery.ttl ${tmpdir}/battery-inferred.ttl
 ontoconvert -s -- ${rootdir}/batteryquantities.ttl ${tmpdir}/batteryquantities-inferred.ttl
 
