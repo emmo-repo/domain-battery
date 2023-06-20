c0fba3d10a94972448f27577327cc50ca32b9eb3
francescalb <francesca.l.bleken@sintef.no>
2023-06-20 07:57:14 +0200

    Removed inferred temporarily

--- doc/mkdoc.sh
+++ doc/mkdoc.sh
@@ -13,8 +13,8 @@ mkdir -p ${tmpdir}/figs
 cp -u ${rootdir}/doc/img/bigmap.png ${tmpdir}/figs/.
 
 ontograph -m ${rootdir}/battery.ttl ${tmpdir}/battery-structure.png
-ontoconvert -si -- ${rootdir}/battery.ttl ${tmpdir}/battery-inferred.ttl
-ontoconvert -si -- ${rootdir}/batteryquantities.ttl ${tmpdir}/batteryquantities-inferred.ttl
+ontoconvert -s -- ${rootdir}/battery.ttl ${tmpdir}/battery-inferred.ttl
+ontoconvert -s -- ${rootdir}/batteryquantities.ttl ${tmpdir}/batteryquantities-inferred.ttl
 
 ontodoc --template=battery.md --format=html ${tmpdir}/battery-inferred.ttl \
         ${tmpdir}/battery.html
